8ed38c5eb52741e471df9cb89a94239e5335e32f
Rinku Kumari <rinku@ideaboard.xyz>
2020-01-16 14:34:51 +0530

    npm package

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/checkList';
-// import App from './example';
+// import App from './src/checkList';
+import App from './example';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);

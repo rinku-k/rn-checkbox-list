3f87504833c163ef5b654f14913c01a01d62f1a5
rinku-k <rinkuk.official@gmail.com>
2021-01-15 22:57:15 +0530

    Fix - item disappear on deselect
    https://github.com/rinku-k/rn-checkbox-list/issues/9

--- src/checkList.js
+++ src/checkList.js
@@ -9,20 +9,23 @@ class CheckboxList extends Component {
     super(props);
     this.state = {
       selectedIndexes: props.selectedListItems.map(item => item.id),
-      selectedListItems: props.selectedListItems,
+      selectedListItems: props.selectedListItems
     };
     this.selectAllItems = this.selectAllItems.bind(this);
   }
-  
+
   shouldComponentUpdate(nextProps, nextState) {
-    this.props.onChange({ ids: nextState.selectedIndexes, items: nextState.selectedListItems });
+    this.props.onChange({
+      ids: nextState.selectedIndexes,
+      items: nextState.selectedListItems
+    });
     return true;
   }
 
   unSelectAllItem() {
     this.setState({
       selectedIndexes: [],
-      selectedListItems: [],
+      selectedListItems: []
     });
   }
 
@@ -32,7 +35,7 @@ class CheckboxList extends Component {
     if (selectedIndexes.length < listItems.length) {
       this.setState({
         selectedIndexes: listItems.map(item => item.id),
-        selectedListItems: listItems,
+        selectedListItems: [...listItems]
       });
     } else {
       this.unSelectAllItem();
@@ -40,50 +43,67 @@ class CheckboxList extends Component {
   }
 
   selectCurrentItem(data) {
-    const { selectedIndexes, selectedListItems } = this.state;
-    const currentHeaderIds = selectedIndexes;
-    const currentHeaderItems = selectedListItems;
-    const currentSelectedIndex = currentHeaderIds.indexOf(data.id);
+    const {
+      selectedIndexes: currentSelectedIds,
+      selectedListItems: currentSelectedItems
+    } = this.state;
+    const currentSelectedIndex = currentSelectedIds.indexOf(data.id);
     if (currentSelectedIndex > -1) {
-      currentHeaderIds.splice(currentSelectedIndex, 1);
-      currentHeaderItems.splice(currentSelectedIndex, 1);
+      currentSelectedIds.splice(currentSelectedIndex, 1);
+      currentSelectedItems.splice(currentSelectedIndex, 1);
     } else {
-      currentHeaderIds.push(data.id);
-      currentHeaderItems.push(data);
+      currentSelectedIds.push(data.id);
+      currentSelectedItems.push(data);
     }
-    this.setState({ selectedIndexes: currentHeaderIds, selectedListItems: currentHeaderItems });
+    this.setState({
+      selectedIndexes: currentSelectedIds,
+      selectedListItems: currentSelectedItems
+    });
   }
 
   render() {
-    const { listItems, headerName, listItemStyle, checkboxProp, headerStyle, theme, onLoading } = this.props;
+    const {
+      listItems,
+      headerName,
+      listItemStyle,
+      checkboxProp,
+      headerStyle,
+      theme,
+      onLoading
+    } = this.props;
     const { selectedIndexes } = this.state;
     return (
       <View style={{ flex: 1 }}>
-        { !!headerName &&
+        {!!headerName && (
           <CheckListHeader
             theme={theme}
-            isActive={selectedIndexes.length === listItems.length && listItems.length > 0}
+            isActive={
+              selectedIndexes.length === listItems.length &&
+              listItems.length > 0
+            }
             text={headerName}
             onPress={this.selectAllItems}
             style={headerStyle}
             checkboxProp={checkboxProp}
           />
-        }
-        { !listItems.length ? onLoading() :
-        <ScrollView>
-          { listItems.map((data) => (
-            <CheckListItem
-              theme={theme}
-              key={`${data.id}`}
-              isActive={selectedIndexes.indexOf(data.id) > -1}
-              text={data.name}
-              onPress={() => this.selectCurrentItem(data)}
-              checkboxProp={checkboxProp}
-              style={listItemStyle}
-            />
-          ))}
-        </ScrollView>
-        }
+        )}
+        {!listItems.length ? (
+          onLoading()
+        ) : (
+          <ScrollView>
+            {listItems.map(data => (
+              <CheckListItem
+                theme={theme}
+                key={`${data.id}`}
+                isActive={selectedIndexes.indexOf(data.id) > -1}
+                text={data.name}
+                onPress={() => this.selectCurrentItem(data)}
+                checkboxProp={checkboxProp}
+                style={listItemStyle}
+              />
+            ))}
+          </ScrollView>
+        )}
       </View>
     );
   }
@@ -98,7 +118,7 @@ CheckboxList.propTypes = {
   headerStyle: PropTypes.object,
   onChange: PropTypes.func,
   onLoading: PropTypes.func,
-  theme: PropTypes.string,
+  theme: PropTypes.string
 };
 
 CheckboxList.defaultProps = {
@@ -110,8 +130,7 @@ CheckboxList.defaultProps = {
   headerStyle: {},
   onChange: () => {},
   onLoading: () => null,
-  theme: '#1A237E',
+  theme: '#1A237E'
 };
 
 export default CheckboxList;
-

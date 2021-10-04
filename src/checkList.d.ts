f4822a7ba2858ebd778623955010e99c3e501167
rinku-k <rinkuk.official@gmail.com>
2021-10-04 12:13:42 +0530

    type definitions added
    fixup

---
+++ src/checkList.d.ts
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleProp, TextStyle, ViewStyle, ColorValue } from 'react-native';
+
+export interface CheckBoxProps {
+  listItems?: listItem[];
+  selectedListItems?: listItem[];
+  headerName?: string;
+  listItemStyle?: StyleProp<ViewStyle>;
+  checkboxProp?: object;
+  headerStyle?: StyleProp<ViewStyle>;
+  onChange?: () => void;
+  onLoading?: () => void;
+  theme?: StyleProp<ColorValue>;
+}
+
+export interface listItem {
+  id?: number | string;
+  name?: string;
+}
+
+export default class CheckBoxList extends React.Component<CheckBoxProps> {}

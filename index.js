6b92fc214bef9507acbfaa98a976c2d8899a9850
rinku-k <rinkuk.official@gmail.com>
2021-10-25 16:31:42 +0530

    v1.1.0

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './example/basic';
+// import App from './example/basic';
+import App from './example/customCheckList';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
